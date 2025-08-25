6d66e38a920b31ca9bb34fa8fadb79a45bf37c72
mjlslink <mjlslink5@gmail.com>
2025-08-25 15:31:59 -0500

    complete

--- java/com/encl/loans/controller/LoansController.java
+++ java/com/encl/loans/controller/LoansController.java
@@ -1,13 +1,22 @@
 package com.encl.loans.controller;
 
 import com.encl.loans.contants.LoansConstants;
+import com.encl.loans.dto.ErrorResponseDto;
+import com.encl.loans.dto.LoansContactInfoDto;
 import com.encl.loans.dto.LoansDto;
 import com.encl.loans.dto.ResourceDto;
 import com.encl.loans.service.ILoansService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -15,11 +24,24 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/loans")
-@AllArgsConstructor
 @Validated
 public class LoansController {
 
-    private ILoansService loansService;
+    private final ILoansService loansService;
+
+    public LoansController(ILoansService iLoansService) {
+        this.loansService = iLoansService;
+    }
+
+    @Value("${build.version}")
+    private String buildVersion;
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private LoansContactInfoDto contactInfoDto;
+
 
     @PostMapping("/create")
     public ResponseEntity<ResourceDto> createLoan(@Valid @NotEmpty
@@ -48,6 +70,57 @@ public class LoansController {
         else
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(new ResourceDto(LoansConstants.STATUS_417, LoansConstants.MESSAGE_417_UPDATE));
+    }
+
+    @GetMapping("/build-info")
+    public ResponseEntity<String> getBuidInfo() {
+        return ResponseEntity.status(HttpStatus.OK).body("Build Version: " + buildVersion);
+    }
+
+    @Operation(
+            summary = "Java version",
+            description = "REST API to fetch the Java version the service uses"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion() {
+        return ResponseEntity.status(HttpStatus.OK).body(environment.getProperty("JAVA_HOME"));
+    }
+
+    @Operation(
+            summary = "Contact Info",
+            description = "REST API to fetch the contact information"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/contact-info")
+    public ResponseEntity<LoansContactInfoDto> getContactInfo() {
+        return ResponseEntity.status(HttpStatus.OK).body(contactInfoDto);
     }
 
     @DeleteMapping("/delete")
