03ec374c9b04362337a3c35b705a4328527a0d40
mjlslink <mjlslink5@gmail.com>
2025-08-27 12:53:34 -0500

    Actuator built in

--- java/com/encl/loans/dto/LoansContactInfoDto.java
+++ java/com/encl/loans/dto/LoansContactInfoDto.java
@@ -2,7 +2,15 @@ package com.encl.loans.dto;
 
 import java.util.List;
 import java.util.Map;
+
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix="loans")
-public record LoansContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {}
+@Getter @Setter
+public class LoansContactInfoDto{
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+}
