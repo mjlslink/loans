6b654873d90226e564255eb3ef652e79319c26ab
mjlslink <mjlslink5@gmail.com>
2025-09-02 12:04:51 -0500

    Actuator built in

---
+++ src/test/java/com/encl/loans/Test.java
@@ -0,0 +1,12 @@
+package com.encl.loans;
+
+
+public class Test {
+
+    @org.junit.jupiter.api.Test
+    public void testMe() {
+        int[] i = {1,2,3,4,5};
+        System.out.println(i);
+    }
+
+}
